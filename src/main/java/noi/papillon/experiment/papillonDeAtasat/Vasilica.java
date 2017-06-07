031f664dbd06bc4ebd4e42c1c8ea4326fb1fd850
StefanTLS <tudureanstefan@gmail.com>
2017-06-07 20:58:54 +0300

    vasilica zice...

---
+++ src/main/java/noi/papillon/experiment/papillonDeAtasat/Vasilica.java
@@ -0,0 +1,17 @@
+package noi.papillon.experiment.papillonDeAtasat;
+
+/**
+ * Created by Stefan on 07.06.2017.
+ */
+public class Vasilica {
+
+    public void vasilica(){
+
+
+
+        System.out.println("Vasilica ataseaza papillonu!!");
+
+    }
+
+
+}
