142ef13fc583c8c251308d791504fdb4338e0cd6
StefanTLS <tudureanstefan@gmail.com>
2017-06-07 20:28:23 +0300

    initial exe

---
+++ src/main/java/noi/papillon/experiment/Main.java
@@ -0,0 +1,14 @@
+package noi.papillon.experiment;
+
+/**
+ * Created by Stefan on 07.06.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("---------------------Main Start---------------------");
+
+    }
+
+
+}
---
+++ src/main/java/noi/papillon/experiment/secondPack/ClasaTest1.java
@@ -0,0 +1,11 @@
+package noi.papillon.experiment.secondPack;
+
+/**
+ * Created by Stefan on 07.06.2017.
+ */
+public class ClasaTest1 {
+
+    private String traLaLa;
+
+
+}
